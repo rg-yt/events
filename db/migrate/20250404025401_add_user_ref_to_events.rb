c541193541a2eddc0ca21ea1da82cabe002aa0e5
rg-yt <103154533+rg-yt@users.noreply.github.com>
2025-04-03 23:34:24 -0400

    Update migration

--- db/migrate/20250404025401_add_user_ref_to_events.rb
+++ db/migrate/20250404025401_add_user_ref_to_events.rb
@@ -1,5 +1,5 @@
 class AddUserRefToEvents < ActiveRecord::Migration[8.0]
   def change
-    add_reference :events, :creator, null: false, foreign_key: { to_table: :users } 
+    add_reference :events, :creator, null: false, foreign_key: { to_table: :users }
   end
 end
