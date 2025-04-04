1c3da5b29099a511be3d71542f53b91f33cbf3f6
rg-yt <103154533+rg-yt@users.noreply.github.com>
2025-04-04 14:08:10 -0400

    Update events controller index and show actions

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,10 @@
 class EventsController < ApplicationController
   before_action :authenticate_user!
   def index
-    @events = Event.all
+    @events = current_user.events.all
+  end
+  
+  def show
+    @event = Event.find(params[:id])
   end
 end
