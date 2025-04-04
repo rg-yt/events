a97b463fe7dacdef6cef713a31043e77eff79d53
rg-yt <103154533+rg-yt@users.noreply.github.com>
2025-04-04 13:21:12 -0400

    Create user reference on events table

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_04_022127) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_04_165320) do
   create_table "events", force: :cascade do |t|
     t.string "location"
     t.date "date"
     t.time "time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "creator_id", null: false
+    t.index ["creator_id"], name: "index_events_on_creator_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -30,4 +32,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_04_022127) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "events", "users", column: "creator_id"
 end
