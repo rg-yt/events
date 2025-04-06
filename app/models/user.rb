3452d74b2531e79492ab2e3e9af83afcfae98f7c
rg-yt <103154533+rg-yt@users.noreply.github.com>
2025-04-05 21:27:07 -0400

    Create and update models to allow event attendance

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :events, foreign_key: :creator_id, inverse_of: "creator", dependent: :destroy
+  has_many :created_events, foreign_key: :creator_id, class_name: "Event", inverse_of: "creator", dependent: :destroy
+  has_many :event_attendances
+  has_many :attended_events, through: :event_attendances, source: :event
 end
