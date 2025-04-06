4c52481697ae163229eba788db2b703b082dfc01
rg-yt <103154533+rg-yt@users.noreply.github.com>
2025-04-05 21:27:30 -0400

    Add through table migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_04_165320) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_06_010805) do
+  create_table "event_attendances", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "event_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_event_attendances_on_event_id"
+    t.index ["user_id"], name: "index_event_attendances_on_user_id"
+  end
+
   create_table "events", force: :cascade do |t|
     t.string "location"
     t.date "date"
@@ -33,5 +42,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_04_165320) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "event_attendances", "events"
+  add_foreign_key "event_attendances", "users"
   add_foreign_key "events", "users", column: "creator_id"
 end
