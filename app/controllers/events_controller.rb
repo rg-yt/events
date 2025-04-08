03e783fa07202885a37a1b6897c23ceaaf54f98e
rg-yt <103154533+rg-yt@users.noreply.github.com>
2025-04-07 21:14:46 -0400

    Update events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,7 @@
 class EventsController < ApplicationController
   before_action :authenticate_user!
   def index
-    @events = current_user.events.all
+    @events = current_user.created_events.all
   end
 
   def show
@@ -13,7 +13,7 @@ class EventsController < ApplicationController
   end
 
   def create
-    @event = current_user.events.build(event_params)
+    @event = current_user.created_events.build(event_params)
 
     if @event.save
       redirect_to @event
